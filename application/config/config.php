4987cb4161eaab7984ac6d4e77f77f9bb7d687b2
Samuel Felipe <samfelgar@gmail.com>
2018-11-21 19:27:25 -0200

    Alteração de domínio base

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://aabbgarimpo1.websiteseguro.com/';
+$config['base_url'] = 'http://aabbgarimpo.com.br/';
 
 /*
 |--------------------------------------------------------------------------
