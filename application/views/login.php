817feacaa8d34227db1d3e45f81c487b287a3ed7
Samuel Felipe <samfelgar@gmail.com>
2018-11-21 20:16:22 -0200

    Login responsivo

--- application/views/login.php
+++ application/views/login.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>AABB</title>
   <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
   <link rel="stylesheet" href="<?= base_url('assets/css/login.css'); ?>">
